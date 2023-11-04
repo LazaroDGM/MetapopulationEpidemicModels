a329e119f701ce5ed730d7736c9f390f6bb524d4
LazaroDGM <lazarodaniel.glezmtnz@gmail.com>
2023-11-04 14:31:16 -0400

    add COMPARTMENTAL_Model class

--- models/SODE_model.py
+++ models/SODE_model.py
@@ -1,17 +1,25 @@
 import numba_models as nm
 import numpy as np
 from scipy.integrate import solve_ivp
+import matplotlib.pyplot as plt
 
 class SODE_Model():
 
-    def __init__(self) -> None:
+    def __init__(self, params, params_name, name= 'SEDO', params_correct= False) -> None:
+
+        if len(params) != len(params_name):
+            raise Exception('La cantidad de parametros y nombres asociados no coincide')
+
+        self._params = params
+        self._params_name = params_name
+        self._params_correct = params_correct
+        self._name_model = name
+        
+
         self._y0 = None
         self._t0 = None
-        self._params = None
-        self._params_name = None
         self._result = None
 
-        self._name_model = 'SEDO'
 
     def fun_SODE(self, t, y, *args) -> np.ndarray:
         '''
@@ -36,6 +44,8 @@ class SODE_Model():
         '''
         Resuelve el modelo numericamente
         '''
+        if not self._params_correct:
+            raise Exception('Los parametros del modelo son Incorrectos')
         try:
             result_sol = solve_ivp(
                 fun= self.fun_SODE,
@@ -48,18 +58,82 @@ class SODE_Model():
             return result_sol
         except Exception as e:
             self._result = None
-            return e
+            raise e
     
     def plot_result(self):
         raise NotImplementedError()
 
 
-    def __str__(self) -> str:
+    def __repr__(self) -> str:
         text = f'Modelo: {self._name_model}\nParametros:\n'
         for i in range(len(self._params)):
-            text += f'\t- {self._params_name}: '
+            text += f'\t- {self._params_name[i]}: '
             text += f'{self._params[i]}\n'
         text += f'Valor Inicial:\n' + \
-        f'\t- t0: {self.t0}\n' + \
-        f'\t- y0: {self.y0}\n'
+        f'\t- t0: {self._t0}\n' + \
+        f'\t- y0: {self._y0}\n'
+        return text
 
+
+class COMPARTMENTAL_Model(SODE_Model):
+
+    def __init__(self, compartmentals_name, compartmentals_short_name, params_name, name) -> None:
+        super().__init__([None] * len(params_name), params_name, name, False)
+
+        if len(compartmentals_name) != len(compartmentals_short_name):
+            raise Exception('Por cada compartimento debe haber un nombre y un nombre corto')
+        
+        if len(compartmentals_name) != len(set(compartmentals_name)):
+            raise Exception('Nombres de compartimentos repetidos')
+        
+        if len(compartmentals_short_name)!= len(set(compartmentals_short_name)):
+            raise Exception('Nombres Cortos de compartimentos repetidos')
+        
+        
+        self.__compartmentals_name = compartmentals_name
+        self.__compartmentals_short_name = [str.lower(i) for i in compartmentals_short_name]
+        self.__total_compartmentals = len(compartmentals_name)
+
+    def set_params(self, params: dict|list):
+        try:
+            if params is dict:
+                for i, name in enumerate(self._params_name):
+                    self._params[i] = params[name]
+                self._params_correct = True
+            else:
+                for i in range(len(self._params_name)):
+                    self._params[i] = params[i]
+                self._params_correct = True
+        except Exception as e:
+            self._params_correct = False
+            raise(e)
+
+    def plot_result(self,mode='total',
+                    xlabel='Tiempo (dias)',
+                    ylabel= 'Cantidad de personas (unidades)',
+                    show= True,
+                    grid= True):
+
+        if self._result is not None:
+            mode = mode.lower()
+            t = self._result.t
+            ys = self._result.y
+            if mode == 'total':
+                for i, y in enumerate(ys):
+                    plt.plot(t, y, label= self.__compartmentals_name[i])
+            else:
+                try:
+                    index = self.__compartmentals_short_name.index(mode)
+                    plt.plot(t, ys[index], label= self.__compartmentals_name[index])
+                except:
+                    raise Exception('Modo de graficado incorrecto')
+            if show:
+                plt.xlabel(xlabel)
+                plt.ylabel(ylabel)
+                plt.title(label=self._name_model)
+                if grid:
+                    plt.grid()
+                plt.legend()
+                plt.show()
+        else:
+            raise Exception('El modelo no esta resuelto.')
--- models/clasic_epidemic_models.py
+++ models/clasic_epidemic_models.py
@@ -1,188 +1,46 @@
-from SODE_model import SODE_Model
+from SODE_model import COMPARTMENTAL_Model
 import numpy as np
 import numba_models as nm
 import matplotlib.pyplot as plt
 
-class SIR_Model(SODE_Model):
+class SIR_Model(COMPARTMENTAL_Model):
 
     def __init__(self) -> None:
-        super().__init__()
-        self._params = [None]*3
-        self._params_name = ['Beta', 'Gamma', 'N']
-        self._params_correct = False
-
-        self._name_model = 'SIR'
-
-    def set_params(self, params: dict):
-        try:
-            if params is dict:
-                self._params[0] = params['Beta']
-                self._params[1] = params['Gamma']
-                self._params[2] = params['N']
-                self._params_correct = True
-            else:
-                self._params[0] = params[0]
-                self._params[1] = params[1]
-                self._params[2] = params[2]
-                self._params_correct = True
-        except Exception as e:
-            self._params_correct = False
-            raise(e)
+        super().__init__(['Susceptibles', 'Infestados', 'Recuperados'],
+                        ['S', 'I', 'R'], ['Beta', 'Gamma', 'N'],'SIR')
         
     def fun_SODE(self, t, y) -> np.ndarray:
-        if self._params_correct:
-            return nm.fun_SIR_SODE(t,y,
-                beta= self._params[0],
-                gamma= self._params[1],
-                N= self._params[2],
-            )
-        else:
-            raise Exception('Los parametros del modelo son Incorrectos')
-        
-    def plot_result(self,mode='t', show= True):
-        if self._result is not None:
-            mode = mode.lower()
-            t = self._result.t
-            ys = self._result.y
-            if mode == 't':
-                for y in self._result.y:
-                    plt.plot(t, y)
-            elif mode == 's':
-                plt.plot(t, ys[0])
-            elif mode == 'i':
-                plt.plot(t, ys[1])
-            elif mode == 'r':
-                plt.plot(t, ys[2])
-            else:
-                raise Exception('Modo de graficado incorrecto')
-            if show:
-                plt.show()
-        else:
-            raise Exception('El modelo no esta resuelto.')
-    
-    def set_initial_value(self, t0, y0):
-        super().set_initial_value(t0, y0)
+        return nm.fun_SIR_SODE(t,y,
+            beta= self._params[0],
+            gamma= self._params[1],
+            N= self._params[2],
+        )
 
 
-class SIS_Model(SODE_Model):
+class SIS_Model(COMPARTMENTAL_Model):
 
     def __init__(self) -> None:
-        super().__init__()
-        self._params = [None]*3
-        self._params_name = ['Beta', 'Gamma', 'N']
-        self._params_correct = False
-
-        self._name_model = 'SIS'
-
-    def set_params(self, params: dict):
-        try:
-            if params is dict:
-                self._params[0] = params['Beta']
-                self._params[1] = params['Gamma']
-                self._params[2] = params['N']
-                self._params_correct = True
-            else:
-                self._params[0] = params[0]
-                self._params[1] = params[1]
-                self._params[2] = params[2]
-                self._params_correct = True
-        except Exception as e:
-            self._params_correct = False
-            raise(e)
+        super().__init__(['Susceptibles', 'Infestados'],
+                        ['S', 'I'], ['Beta', 'Gamma', 'N'],'SIS')
         
     def fun_SODE(self, t, y) -> np.ndarray:
-        if self._params_correct:
-            return nm.fun_SIS_SODE(t,y,
-                beta= self._params[0],
-                gamma= self._params[1],
-                N= self._params[2],
-            )
-        else:
-            raise Exception('Los parametros del modelo son Incorrectos')
-        
-    def plot_result(self,mode='t', show= True):
-        if self._result is not None:
-            mode = mode.lower()
-            t = self._result.t
-            ys = self._result.y
-            if mode == 't':
-                for y in self._result.y:
-                    plt.plot(t, y)
-            elif mode == 's':
-                plt.plot(t, ys[0])
-            elif mode == 'i':
-                plt.plot(t, ys[1])
-            else:
-                raise Exception('Modo de graficado incorrecto')
-            if show:
-                plt.show()
-        else:
-            raise Exception('El modelo no esta resuelto.')
-    
-    def set_initial_value(self, t0, y0):
-        super().set_initial_value(t0, y0)
+        return nm.fun_SIS_SODE(t,y,
+            beta= self._params[0],
+            gamma= self._params[1],
+            N= self._params[2],
+        )
 
-class SEIR_Model(SODE_Model):
+class SEIR_Model(COMPARTMENTAL_Model):
 
     def __init__(self) -> None:
-        super().__init__()
-        self._params = [None]*4
-        self._params_name = ['Beta', 'Gamma', 'N']
-        self._params_correct = False
-
-        self._name_model = 'SIS'
-
-    def set_params(self, params: dict):
-        try:
-            if params is dict:
-                self._params[0] = params['Beta']
-                self._params[1] = params['Gamma']
-                self._params[2] = params['Sigma']
-                self._params[3] = params['N']
-                self._params_correct = True
-            else:
-                self._params[0] = params[0]
-                self._params[1] = params[1]
-                self._params[2] = params[2]
-                self._params[3] = params[3]
-                self._params_correct = True
-        except Exception as e:
-            self._params_correct = False
-            raise(e)
+        super().__init__(['Susceptibles', 'Expuestos', 'Infestados', 'Recuperados'],
+                        ['S', 'E', 'I', 'R'], ['Beta', 'Gamma', 'Sigma', 'N'],'SEIR')
         
     def fun_SODE(self, t, y) -> np.ndarray:
-        if self._params_correct:
-            return nm.fun_SEIR_SODE(t,y,
-                beta= self._params[0],
-                gamma= self._params[1],
-                sigma= self._params[2],
-                N= self._params[3],
-            )
-        else:
-            raise Exception('Los parametros del modelo son Incorrectos')
-        
-    def plot_result(self,mode='t', show= True):
-        if self._result is not None:
-            mode = mode.lower()
-            t = self._result.t
-            ys = self._result.y
-            if mode == 't':
-                for y in self._result.y:
-                    plt.plot(t, y)
-            elif mode == 's':
-                plt.plot(t, ys[0])
-            elif mode == 'e':
-                plt.plot(t, ys[1])
-            elif mode == 'i':
-                plt.plot(t, ys[2])
-            elif mode == 'r':
-                plt.plot(t, ys[3])
-            else:
-                raise Exception('Modo de graficado incorrecto')
-            if show:
-                plt.show()
-        else:
-            raise Exception('El modelo no esta resuelto.')
-    
-    def set_initial_value(self, t0, y0):
-        super().set_initial_value(t0, y0)
+        return nm.fun_SEIR_SODE(t,y,
+            beta= self._params[0],
+            gamma= self._params[1],
+            sigma= self._params[2],
+            N= self._params[3],
+        )
+
